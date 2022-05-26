a26f12f69f5c9ea930ab2c146c730c04dc5bd95b
techipps <100496746+techipps@users.noreply.github.com>
2022-05-25 19:40:09 -0700

    first

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
 # Project 4: Around The U.S.
 
 ### Overview
+
 Project created with the following technologies
 
-* Figma
-* HTML
-* CSS
-* Javascript
+- Figma
+- HTML
+- CSS
+- Javascript
 
 **Figma**
 
-* [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
+- [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
 
 ### First Project using JavaScript :) :)
+
 **Vist My Website**
-* [Link to the project ](https://bilal2786.github.io/web_project_4/)
+
+- [Link to the project ](https://techipps.github.io/se_project_aroundtheus/)
 
 **HAVE FUN**
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,32 @@
-const editModalEl = document.querySelector(".button__edit") ;
-const editModalNameInput = document. querySelector(".js-modal-name" );
-const editModalTitleInput = document. querySelector(".js-modal-title");
+const editModalEl = document.querySelector(".button__edit");
+const editModalNameInput = document.querySelector(".js-modal-name");
+const editModalTitleInput = document.querySelector(".js-modal-title");
+const saveModalEl = document.querySelector(".modal__save");
 
-const modalCloseBtnEl = document.querySelector(".button__close");
+const modalCloseBtnEl = document.querySelector(".modal__close");
 
 const modalEl = document.querySelector(".modal");
 
 const profileNameEl = document.querySelector(".profile__name");
-const profileTitleEl = document.querySelector(".profile__title");
+const profileTitleEl = document.querySelector(".profile__about");
 
-function(modalCloseBtnEl) {
-    modalEl.classList.remove("modal__open");
-
+function modalClose() {
+  modalEl.classList.remove("modal__open");
 }
 
-cardEditBtnEl.addEventListener("click", function () {
-modalEl.classList.add("modal__open");
+editModalEl.addEventListener("click", function () {
+  modalEl.classList.add("modal__open");
 });
-modalCloseBtnEl. addEventListener("click", function () {
+modalCloseBtnEl.addEventListener("click", function () {
+  modalClose;
 });
 
 editModalNameInput.value = profileNameEl.textContent;
-editModalTitleInput. value = profileTitleEl.textContent;
+editModalTitleInput.value = profileTitleEl.textContent;
 
-editModalEl addEventlistener("submit", function (event) {
-event.preventDefault();
-profileNameEl.textContent = editFormNameInput.value;
-profileTitleEl.textContent = editFormTitleInput.value;
+editModalEl.addEventlistener("submit", function (event) {
+  event.preventDefault();
+  profileNameEl.textContent = editFormNameInput.value;
+  profileTitleEl.textContent = editFormTitleInput.value;
+  modalClose;
 });
